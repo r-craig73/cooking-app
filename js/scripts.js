65c47c4eaa49d593614b113e5b0c051cd08025b6
Ron Craig <ron.craig@comcast.net>
2018-08-23 23:42:01 -0700

    edit Javascript indentation and add precision to 3 digits

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var myTablespoons = parseInt(prompt("How many tablespoons?"));
 
-var newCups = function(inputTbsp) {return "You have " + (inputTbsp) * (1/16) + " cups";
+var newCups = function(inputTbsp) {
+  return "You have " + ((inputTbsp) * (1/16)).toPrecision(4) + " cups";
 };
 
 alert(newCups(myTablespoons));
